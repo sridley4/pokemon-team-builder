69d9b34d05fa3f53fdc35faf7320a16b7199bca0
ST Ridley <sridley4@uwo.ca>
2020-10-19 23:09:46 -0400

    login and api functionality added

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
+    apiGateway: {
+      REGION: "YOUR_API_GATEWAY_REGION",
+      URL: "YOUR_API_GATEWAY_URL"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_67OAoprr7",
+      APP_CLIENT_ID: "515594odbsebsi491fjjb9q7rn",
+      IDENTITY_POOL_ID: "930c34cc-c3c4-42df-bcd7-4d73496ed327"
+    }
+};
