5d6d63eb953ad893169b27451a4323d8b117d3e1
repl.it user <replituser@example.com>
2020-10-25 02:08:56 +0000

    changing the config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 export default {
     apiGateway: {
       REGION: "us-east-2",
-      URL: "https://sq7knxvixi.execute-api.us-east-2.amazonaws.com/dev"
+      URL: "https://8o0d4k8bj2.execute-api.us-east-2.amazonaws.com/dev"
     },
     cognito: {
       REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_bYsaH7MEK",
-      APP_CLIENT_ID: "7h21agqojrlnjn2fhbdna60",
-      IDENTITY_POOL_ID: "us-east-2:6c178925-c41d-4d8e-9f92-03fdc2b8cb4c"
+      USER_POOL_ID: "us-east-2_zykdGvqUb",
+      APP_CLIENT_ID: "8ppcn2u18pgasu2kjoq8t5hlp",
+      IDENTITY_POOL_ID: "us-east-2:16cd6b7c-df32-4675-841d-83a4f6703a52"
     }
 };
